b6cf612c6cb582a5df16256517bcb5a0dd15a2e1
Oran <2746214G@student.gla.ac.uk>
2024-02-05 23:28:46 +0000

    git add * git commit -m "halfway through 9" git push

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from rango.models import Page, Category
+from rango.models import Page, Category, UserProfile
+from django.contrib.auth.models import User
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
@@ -16,17 +17,23 @@ class PageForm(forms.ModelForm):
     url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
-    #def clean(self):
-    #    cleaned_data = self.cleaned_data
-    #    url = cleaned_data.get('url')
-    #    if url and not url.startswith('http://'):
-    #        url = f'http://{url}'
-    #        cleaned_data['url'] = url
-    #    return cleaned_data
-
     class Meta:
         model = Page
         exclude = ('category',)
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+class UserProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
+
 
 
 
